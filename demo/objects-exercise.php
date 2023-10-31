a94577709c7dd7c62c6d90fa3441220df7eee66a
andy vo <andy.vo170@hotmail.com>
2023-10-31 18:45:47 -0400

    added properties and action

--- demo/objects-exercise.php
+++ demo/objects-exercise.php
@@ -5,10 +5,14 @@ ini_set('display_errors', 1);
 class Umbrella
 {
   private $isOpen;
+  private $color;
+  private $isBroken;
 
-  public function __construct()
+  public function __construct($color, $isBroken)
   {
     $this->isOpen = false;
+    $this->color = $color;
+    $this->isBroken = $isBroken;
   }
 
   public function open()
@@ -21,36 +25,54 @@ class Umbrella
     $this->isOpen = false;
   }
 
+  public function clickButton()
+  {
+    $this->open();
+  }
+
+  public function pushToClose()
+  {
+    $this->close();
+  }
+
+  public function action($openOrClose)
+  {
+    if ($openOrClose == 'click') {
+      $this->open();
+    } elseif ($openOrClose == 'push') {
+      $this->close();
+    } else {
+      echo "its broken";
+    }
+  }
+
+  public function getColor()
+  {
+    //must include return in order to "return" the value
+    return $this->color;
+  }
+  //if you don't use return, it will still perform the operations, but it won't return a value.
+
+  public function getIsBroken()
+  {
+    return $this->isBroken;
+  }
+
   public function getStatus()
   {
     if ($this->isOpen) {
-      echo "umbrella is open.";
+      echo "action: button clicked. <br>";
+      //must include $this-> in order to get properties
+      echo "my " . $this->isBroken . " " . $this->color . " umbrella is open.";
     } else {
-      echo "umbrella is closed.";
+      echo "action: push down umbrella. <br>";
+      echo "my " . $this->isBroken . " " . $this->color . " umbrella is closed.";
     }
   }
 }
 
 
-$myUmbrella = new Umbrella();
+$myUmbrella = new Umbrella("yellow", "not so broken");
 
-$myUmbrella->open();
-// $myUmbrella->close();
+$myUmbrella->action('click');
 $myUmbrella->getStatus();
-
-
-/*
-
-Object: Umbrella
-
-Properties:
-- isOpen?
-- isBroken?
-
-Methods:
-- button for open
-- push to close
-
-
-
-*/
